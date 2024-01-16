e9dfa38a7c5d3ad3656e5562eecf02f1d0c472a0
ghcollin <16874712+ghcollin@users.noreply.github.com>
2024-01-16 16:48:08 +1030

    Added setup py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='healjax',
+    version='0.1.0',
+    url='https://github.com/ghcollin/healjax',
+    description='Healpix routines for JAX.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    keywords='jax healpix healpy',
+    license='MIT',
+    author='ghcollin',
+    author_email='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=['healjax'],
+    install_requires=['numpy']
+)
